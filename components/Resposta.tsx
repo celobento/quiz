c190fb6d9fd00e9a38862bbcb53118bf1a0ff5d5
Marcelo Bento <celobento26@gmail.com>
2024-09-03 22:24:09 -0300

    animating answer

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -10,11 +10,12 @@ interface RespostaProps {
 }
 export default function Resposta(props: RespostaProps) {
     const resposta = props.valor  
+    const respostaRevelada = resposta.revelada ? styles.respostaRevelada : ''
     return (
         <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>
-            <div className={styles.conteudoResposta}>
-                {!resposta.revelada ? (
-                    <div className={styles.frente}>
+            <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
+                
+                <div className={styles.frente}>
                     <div className={styles.letra}
                     style={{ backgroundColor: props.corFundoLetra}}>
                         {props.letra}
@@ -23,8 +24,8 @@ export default function Resposta(props: RespostaProps) {
                         {resposta.valor}
                     </div>
                 </div>
-                ) : (
-                    <div className={styles.verso}>
+            
+                <div className={styles.verso}>
                     {resposta.certa ? (
                         <div className={styles.certa}>
                             <div> A resposta certa é ...</div>
@@ -37,8 +38,6 @@ export default function Resposta(props: RespostaProps) {
                         </div>
                     )}
                 </div>
-                )}
-                
                 
             </div>
         </div>
